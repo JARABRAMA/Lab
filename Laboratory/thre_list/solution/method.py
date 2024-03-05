934744f4103435a8dee69a338487db5ac7fe4869
Mariana Carvajal <mariana.carvajalr@udea.edu.co>
2024-03-05 12:08:24 -0500

    Mariana cambios

--- Laboratory/thre_list/solution/method.py
+++ Laboratory/thre_list/solution/method.py
@@ -1,1 +1,9 @@
-def common_elements(a: )
+from Laboratory.common_classes.Classes import List, Node
+
+
+def common_elements(a:List, b:List, c:List )->List:
+    an: Node = a.get_head()
+    bn: Node = b.get_head()
+    cn: Node = c.get_head()
+
+
