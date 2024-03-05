8640c91097cb740dd8948afaa4131a1df533d293
JARABRAMA <brayanjaraba2006@gmail.com>
2024-03-05 13:13:02 -0500

    gracias mariana ya funciona el metodo common_elents

--- Laboratory/thre_list/solution/method.py
+++ Laboratory/thre_list/solution/method.py
@@ -2,17 +2,14 @@ from Laboratory.common_classes.Classes import List, Node
 
 
 def common_elements(a: List, b: List, c: List) -> List:
-    solution: List = List("string")
     init = False
-    an = a.get_head()
-    bn: Node = b.get_head()
-    cn: Node = c.get_head()
 
-    init = False
+    an: Node = a.get_head()
     while an is not None:
+        bn = b.get_head()
         while bn is not None:
-
             if an.get_data() == bn.get_data():
+                cn: Node = c.get_head()
                 while cn is not None:
                     if cn.get_data() == an.get_data():
                         if not init:
@@ -21,7 +18,7 @@ def common_elements(a: List, b: List, c: List) -> List:
                         else:
                             solution.add(an.get_data())
                     cn = cn.get_right_link()
-
             bn = bn.get_right_link()
-        an = an.get_right_link()  # Ultima instruccion
+        an = an.get_right_link()  # Last instruction
+
     return solution
--- Laboratory/thre_list/testing/three_list_testing.py
+++ Laboratory/thre_list/testing/three_list_testing.py
@@ -4,8 +4,8 @@ from Laboratory.thre_list.solution.method import common_elements
 a = List(1)
 a.add(2)
 a.add(3)
-a.add(4)
-a.add(5)
+
+
 
 b = List(2)
 b.add(4)
@@ -19,5 +19,5 @@ c.add(6)
 c.add(7)
 c.add(8)
 
-target = common_elements(a,b,c)
+target = common_elements(a, b, c)
 target.print()
